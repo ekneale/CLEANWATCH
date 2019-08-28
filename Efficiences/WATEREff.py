ed01107ea16c32061bc608bfb2c487ef036afb80
gayyoda <treasdev@gmail.com>
2019-08-28 14:53:09 +0100

    updated water file

---
+++ Efficiences/WATEREff.py
@@ -0,0 +1,11 @@
+#no data yet [28/8/19]
+from statistics import mean
+Pb214 = []
+Bi214 = []
+Tl210 = []
+Bi210 = []
+print('Efficiencies for Gd Water')
+print('Mean Efficiency of Pb214 = %.5e' % mean(Pb214))
+print('Mean Efficiency of Bi214 = %.5e' % mean(Bi214))
+print('Mean Efficiency of Tl210 = %.5e' % mean(Tl210))
+print('Mean Efficiency of Bi210 = %.5e' % mean(Bi210))
