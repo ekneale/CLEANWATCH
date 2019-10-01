732c9aa886b6e495fc6d978439188a5301e038b3
Katie Healey <healey@hep1.shef.ac.uk>
2019-10-01 15:58:27 +0100

    alskdjflas

--- PRootTest.py
+++ PRootTest.py
@@ -4,4 +4,6 @@ import ROOT
 outfile = ROOT.TFile("results.root", "READ")
 hist = outfile.Get("histWatchman_TANK_60Co_STEEL_ACTIVITY")
 binNum = outfile.Get("histWatchman_TANK_60Co_STEEL_ACTIVITY").FindBin(1.6, 13)
+Eff = outfile.Get("histWatchman_TANK_60Co_STEEL_ACTIVITY").GetBinContent(binNum)
 print('Bin Number = ', binNum)
+print('Efficiency = ', Eff)
