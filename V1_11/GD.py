d86814dec80f24e2323278d9c18fb125bc6e77fd
ekneale <eskneale1@sheffield.ac.uk>
2020-02-07 11:21:38 +0000

    corrects order of radioactivities for isotopes in GD

--- V1_11/GD.py
+++ V1_11/GD.py
@@ -4,16 +4,16 @@ import Eff
 import Iso
 TankR = 10026.35e-3
 Height = 10026.35e-3
-defPPM = [10e-3, 0.25e-3, 0.2e-3, 0.28e-3, 1.7e-3, 0.35e-3] #mBq/kg
+defPPM = [10e-3, 0.2e-3, 0.25e-3, 0.28e-3, 0.35e-3, 1.7e-3] #mBq/kg
 IsoAct = defPPM
 revIsoAct = defPPM
 IsoList = Iso.GD
-IsoDecay = [Iso.U238,        #U238
-            Iso.Th232,       #Th232
-            Iso.U235,        #U235
-            Iso.U238,        #U238
-            Iso.Th232,       #Th232
-            Iso.U235]        #U235
+IsoDecay = [Iso.U238,        #U238 upper
+            Iso.Th232,       #Th232 upper
+            Iso.U235,        #U235 upper
+            Iso.U238,        #U238 lower
+            Iso.Th232,       #Th232 lower 
+            Iso.U235]        #U235 lower
 IsoEff =   [Eff.GDU238,      #U238
             Eff.GDTh232,     #Th232
             Eff.GDU235]      #U235
